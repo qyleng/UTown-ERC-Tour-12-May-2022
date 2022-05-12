3031876ccb42644fcc55274b11311a9f4d47f86e
qyleng <105409085+qyleng@users.noreply.github.com>
2022-05-12 16:27:30 +0800

    Utown ERC tour

---
+++ data.js
@@ -0,0 +1,152 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-erc_level-1_nearescalator_photo_1512",
+      "name": "ERC_Level 1_NearEscalator_PHOTO_1512",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.8319748521286385,
+        "pitch": 0.012313549137267898,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3206017061249966,
+          "pitch": 0.1534868623262149,
+          "rotation": 5.497787143782138,
+          "target": "2-erc_level-1_tembusu-tree"
+        },
+        {
+          "yaw": -1.0185850147543576,
+          "pitch": 0.02830691018819209,
+          "rotation": 3.141592653589793,
+          "target": "1-erc_level-1_starbucks"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.411229464189372,
+          "pitch": -0.07799532423329403,
+          "title": "Education Resource Centre",
+          "text": "ERC escalator"
+        }
+      ]
+    },
+    {
+      "id": "1-erc_level-1_starbucks",
+      "name": "ERC_Level 1_Starbucks",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.572305323950175,
+        "pitch": 0.1169239703550673,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8378076538231998,
+          "pitch": -0.2111459349936169,
+          "rotation": 0,
+          "target": "0-erc_level-1_nearescalator_photo_1512"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.9749869432762743,
+          "pitch": -0.128246075148148,
+          "title": "Starbucks",
+          "text": "Starbucks in ERC"
+        }
+      ]
+    },
+    {
+      "id": "2-erc_level-1_tembusu-tree",
+      "name": "ERC_Level 1_Tembusu Tree",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.9855613010774995,
+        "pitch": -0.04925284446165179,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.743120915666996,
+          "pitch": 0.15153656524911696,
+          "rotation": 5.497787143782138,
+          "target": "0-erc_level-1_nearescalator_photo_1512"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.9796310956693404,
+          "pitch": -0.21554966116819685,
+          "title": "Tembusu Tree",
+          "text": "A tree in ERC"
+        }
+      ]
+    }
+  ],
+  "name": "ERC at Utown",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
